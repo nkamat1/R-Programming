c5b95a6dccca22da42136b1aec5d9e3f82976baf
nkamat1 <42752302+nkamat1@users.noreply.github.com>
2018-09-08 15:59:24 -0400

    Update Generating-Graph.R

--- Generating-Graph.R
+++ Generating-Graph.R
@@ -1,3 +1,8 @@
 a<- women
 a
 plot(a$height,a$weight,xlab = 'height of women',ylab = 'weight of women',main = 'average height vs weight ratio')
+median(a$height)
+mode(a$height)
+mean(a$weight)
+median(a$weight)
+
