f63981178a4a3c3592ae7d89705df52c79f2d31e
nkamat1 <42752302+nkamat1@users.noreply.github.com>
2019-01-08 11:38:39 -0500

    Delete Generating-Graph.R

--- Generating-Graph.R
+++
@@ -1,8 +0,0 @@
-a<-read.csv('women.csv')
-a
-plot(a$height,a$weight,xlab = 'height of women',ylab = 'weight of women',main = 'average height vs weight ratio')
-median(a$height)
-mean(a$weight)
-median(a$weight)
-summary(a$height)
-summary(a$weight)
